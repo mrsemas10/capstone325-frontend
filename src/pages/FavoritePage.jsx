06a679fda36c4d2d7dcf9d55c1f8910b345d0947
mrsemas10 <mrsemas10@gmail.com>
2024-08-26 16:36:39 -0400

    updated favorite page

--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,9 +1,22 @@
 import axios from "axios";
-import { backendUrl } from "../utils";
 import SearchResult from "../components/SearchResult";
 
-const FavoritePage = ({ user }) => {
+
+
+  const viewfavorite = async (location) => {
+    const apiKey = "2c8fcaff5cmshb30514913395c4fp10f062jsn47dbd8c865d9";
+    
+    axios
+      .get(`https://weatherapi-com.p.rapidapi.com/current.json?q=${location}`, {
+        headers: {
+          "x-rapidapi-key": apiKey,
+          "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+        },
+      })
+   
+  };
+
   
-};
+
 
 export default FavoritePage;
