60073dc2945217302637f9fc20ebab84474dabed
mrsemas10 <mrsemas10@gmail.com>
2024-08-26 16:15:31 -0400

    updated homepage

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,24 +1,38 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-const Navigation = ({ }) => {
+const Navigation = ({ handleLogout, user }) => {
   return (
     <nav>
-      <div className="">
+      <div className="nav-center">
         <div>
-          <Link to="/" className="">
+          <Link to="/" className="nav-item">
             Home
           </Link>
         </div>
         <div>
-            <div className="">
-              <Link to="/favorite" className="">
+          {user ? (
+            <div className="nav-links-con">
+              <Link to="/favorite" className="nav-item">
                 Favorite
               </Link>
-              <Link to="/search" className="">
+              <Link to="/search" className="nav-item">
                 Search
               </Link>
+              <button className="primary-btn" onClick={handleLogout}>
+                Logout
+              </button>
             </div>
+          ) : (
+            <div className="nav-links-con">
+              <Link to="/login" className="nav-item">
+                Login
+              </Link>
+              <Link to="/register" className="nav-item">
+                Register
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </nav>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,27 @@ import { Link } from "react-router-dom";
 const HomePage = ({ }) => {
   return (
     <>
+      <section className="">
+        <div className="">
+          <p className="">
+            <span>Plan Your Perfect Getaway</span>
+          </p>
+        </div>
+
+        
+          <div>
+            <p className="">Start exploring</p>
+            <div className="">
+              <Link to="/search">
+                <button className="">Search</button>
+              </Link>
+              <Link to="/favorite">
+                <button className="">Favorite</button>
+              </Link>
+            </div>
+          </div>
+        
+      </section>
       
     </>
   );
