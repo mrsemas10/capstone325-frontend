0ea10fe32a9d2fa3a882d7be41a3f8eced21ac4a
mrsemas10 <mrsemas10@gmail.com>
2024-08-28 14:29:05 -0400

    update form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,46 @@
 import React, { useState } from 'react';
 
-export default function Form() {
-    
+export default function Form(props) {
+    const [formData, setFormData] = useState({ searchTerm: "" })
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.onWeatherSearch(formData.searchTerm)
+
+        setFormData({
+            ...formData,
+            searchTerm: "",
+        })
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit} >
+                <input
+                    type='text'
+                    name='searchTerm'
+                    onChange={handleChange}
+                    value={formData.searchTerm}
+                    placeholder='Enter location'
+                    style={{
+                        borderRadius: '10px',
+                        border: 'white',
+                        padding: '5px',
+                        outline: 'solid',
+                        width: '150px',
+                        marginRight: '20px',
+                        marginLeft: '-25px',
+                    }}
+                />
+                <input type="submit" value='Find My Location' style={{ borderRadius: '8px', cursor: 'pointer', padding: '2px'}}/>
+            </form>
+        </div>
+    )
 }
