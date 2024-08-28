c6bb37a679a07273a45b7c9c99b1b97ceeb635b7
mrsemas10 <mrsemas10@gmail.com>
2024-08-28 12:04:51 -0400

    update register page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,8 +1,47 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { backendUrl } from "../utils";
 
-const RegisterPage = ({}) => {
-  
-};
+
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault(); 
+    if (email === "")
+      return setError((prev) => {
+        return { ...prev, emailError: "Email required" };
+      });
+    if (password === "")
+      return setError((prev) => {
+        return { ...prev, passwordError: "Password required" };
+      });
+    if (confirmpassword === "")
+      return setError((prev) => {
+        return { ...prev, confirmpasswordError: "Password don't match" };
+      });
+    if (confirmpassword !== password)
+      return setError((prev) => {
+        return { ...prev, confirmpasswordError: "Password don't match" };
+      });
+    setLoading(true);
+
+    await axios
+      .post(backendUrl + "/auth/register", {
+        email,
+        password,
+      })
+      .then((res) => {
+        if (res.status === 201) {
+          alert("Account created successfully");
+          return navigate("/login");
+        }
+      })
+      .catch((error) => {
+        alert("Email already exists registering");
+      })
+      .finally(() => setLoading(false));
+  };
+
+ 
 
 export default RegisterPage;
