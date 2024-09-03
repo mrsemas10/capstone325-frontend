da9875ad661e6da2a4c5b36189df5807f1bccd5f
mrsemas10 <mrsemas10@gmail.com>
2024-09-03 13:58:45 -0400

    reverting back to original

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
-// export const backendUrl = "http://localhost:5000";
+export const backendUrl = "http://localhost:5000";
 // export const backendUrl = "postgresql://postgres:tFhBddAYYSwuPgCltIlAiywZscGvKwgD@meticulous-empathy.railway.internal:5432/railway"
-export const backendUrl = "https://capstone325-backend.onrender.com"
+// export const backendUrl = "https://capstone325-backend.onrender.com"
